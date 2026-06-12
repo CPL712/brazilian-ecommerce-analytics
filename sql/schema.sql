694f99fce02d9cd40bfff11687b9d94e05ab8518
CPL712 <cheungpangli300@gmail.com>
2026-06-11 17:09:55 -0700

    Create schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,17 @@
+-- Create Dimension Tables
+
+CREATE TABLE DimCustomer (...);
+
+CREATE TABLE DimDate (...);
+
+CREATE TABLE DimOrder (...);
+
+CREATE TABLE DimPayment (...);
+
+CREATE TABLE DimProduct (...);
+
+CREATE TABLE DimSeller (...);
+
+-- Create Fact Table
+
+CREATE TABLE FactOrderItems (...);
