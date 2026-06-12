0d1a6456f68ead299b564cca37b12d1633cdf99b
CPL712 <cheungpangli300@gmail.com>
2026-06-11 17:11:52 -0700

    Create analysis_queries.sql

---
+++ sql/analysis_queries.sql
@@ -0,0 +1,19 @@
+-- Top Selling Categories
+
+SELECT ...
+
+-- Monthly Revenue
+
+SELECT ...
+
+-- Top Customers
+
+SELECT ...
+
+-- Delivery Performance
+
+SELECT ...
+
+-- Payment Method Analysis
+
+SELECT ...
